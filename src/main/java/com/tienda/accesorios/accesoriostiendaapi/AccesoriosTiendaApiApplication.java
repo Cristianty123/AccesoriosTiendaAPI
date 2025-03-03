f176809be14b3b2f65c0b208d3510204c5307086
Cristian <cristianty123@gmail.com>
2025-03-03 11:02:18 -0500

    primer commit

---
+++ src/main/java/com/tienda/accesorios/accesoriostiendaapi/AccesoriosTiendaApiApplication.java
@@ -0,0 +1,13 @@
+package com.tienda.accesorios.accesoriostiendaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesoriosTiendaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccesoriosTiendaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tienda/accesorios/accesoriostiendaapi/AccesoriosTiendaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.accesorios.accesoriostiendaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesoriosTiendaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
