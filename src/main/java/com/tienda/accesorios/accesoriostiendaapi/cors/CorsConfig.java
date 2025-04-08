384c596637462838242b93ab1cf7d90cb8d16ff2
BryanLizcano <bryanlizcano80@gmail.com>
2025-04-07 20:56:47 -0500

    Agregar corsconnfig

---
+++ src/main/java/com/tienda/accesorios/accesoriostiendaapi/cors/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.tienda.accesorios.accesoriostiendaapi.cors;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+}
