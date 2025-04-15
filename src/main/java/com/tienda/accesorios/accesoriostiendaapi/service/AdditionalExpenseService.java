aa9965c2b757b9737d482d108bd5ffbb7f844d48
Cristian <cristianty123@gmail.com>
2025-04-14 22:42:31 -0500

    endpoint para tener todos los gastos adicionales

--- src/main/java/com/tienda/accesorios/accesoriostiendaapi/service/AdditionalExpenseService.java
+++ src/main/java/com/tienda/accesorios/accesoriostiendaapi/service/AdditionalExpenseService.java
@@ -1,9 +1,13 @@
 package com.tienda.accesorios.accesoriostiendaapi.service;
 
+import com.tienda.accesorios.accesoriostiendaapi.dto.AdditionalExpenseResponse;
 import com.tienda.accesorios.accesoriostiendaapi.model.AdditionalExpense;
 import com.tienda.accesorios.accesoriostiendaapi.repository.AdditionalExpenseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AdditionalExpenseService {
@@ -14,8 +18,18 @@ public class AdditionalExpenseService {
     public boolean existsByNameAndExpense(String name, double expense) {
         return repository.findByNameAndExpense(name, expense).isPresent();
     }
+    public List<AdditionalExpenseResponse> getAllAdditionalExpenses() {
+        return repository.findAll().stream()
+                .map(this::mapToDto)
+                .collect(Collectors.toList());
+    }
 
-    public void save(AdditionalExpense expense) {
-        repository.saveAndFlush(expense);
+    private AdditionalExpenseResponse mapToDto(AdditionalExpense entity) {
+        return new AdditionalExpenseResponse(
+                entity.getId(),
+                entity.getName(),
+                entity.getExpense(),
+                entity.getDescription()
+        );
     }
 }
